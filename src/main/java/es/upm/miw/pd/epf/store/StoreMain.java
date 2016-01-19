c53ee50fbf5cae59ac907152abcd412d1b092501
JTENAESCOB001 <jm.tena01@hotmail.com>
2016-01-19 13:23:39 +0100

    Store - Net y Local final

--- src/main/java/es/upm/miw/pd/epf/store/StoreMain.java
+++ src/main/java/es/upm/miw/pd/epf/store/StoreMain.java
@@ -20,7 +20,14 @@ public class StoreMain {
 		//No se puede realizar el test de esta clase porque, como la disponibilidad de la
 		//conexion es aleatoria (y eso no se puede modificar segun el enunciado)
 		//no se puede programar con exactitud una prueba
-		
+		StoreMain store = new StoreMain();
+		store.write("O1", "Objeto 1");
+		store.write("O2", "Objeto 2");
+		store.write("O3", "Objeto 3");
+		System.out.println("Recuperando objetos");
+		System.out.println("Objeto recuperado: " + store.read("O1"));
+		System.out.println("Objeto recuperado: " + store.read("O2"));
+		System.out.println("Objeto recuperado: " + store.read("O3"));
 	}
 	
 }
